52659ea919b1241df40cdc6bcd7f9951dbd5ce17
Asus <singhalokkumar427@gmail.com>
2025-05-17 19:20:56 +0530

    function and method basic

---
+++ src/Method_basic.java
@@ -0,0 +1,14 @@
+public class Method_basic {
+    public static void fun(){
+        for(int i=1; i<=10; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hi, Good morning");
+        fun();
+        System.out.println("ram");
+    }
+}
