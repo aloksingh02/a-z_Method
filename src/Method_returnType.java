fe2e5f01c10a3b9dc6ced98be5496e5b00d29abc
Asus <singhalokkumar427@gmail.com>
2025-05-18 14:30:42 +0530

    basic of returnType

---
+++ src/Method_returnType.java
@@ -0,0 +1,9 @@
+public class Method_returnType {
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int x = sum(5,15);
+        System.out.println(x);
+    }
+}
