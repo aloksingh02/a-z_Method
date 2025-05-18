3cece739f9b795139ecd9d0744d46c41afe3fa01
Asus <singhalokkumar427@gmail.com>
2025-05-18 13:40:33 +0530

    how to give a argument and call it.

---
+++ src/Method_passingArgument.java
@@ -0,0 +1,26 @@
+public class Method_passingArgument {
+//    public static void greet(String name){
+//        System.out.println("good morning "+name);
+//    }
+//    public static void main(String[] args) {
+//        greet("alok singh");
+//
+//    }
+
+    //sum
+//    public static void sum(int a, int b) {
+//        System.out.println(a+b);
+//    }
+//
+//    public static void main(String[] args) {
+//        sum(56, 23);
+//    }
+
+    public static void intro(String name, int age){
+        System.out.println("good morning "+name + " your age is "+age);
+    }
+
+    public static void main(String[] args) {
+        intro("alok", 25);
+    }
+}
