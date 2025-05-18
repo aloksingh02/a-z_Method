ef35990149e811f3b451db458613e08ce416754c
Asus <singhalokkumar427@gmail.com>
2025-05-18 13:49:37 +0530

    Student detail using method

---
+++ src/Method_studentDetail.java
@@ -0,0 +1,8 @@
+public class Method_studentDetail {
+    public static void studDetail(String name, int age, int classSection, String grade){
+        System.out.println("My name is "+ name +"my age is "+age +" i am in class " +classSection+ " i secure "+ grade);
+    }
+    public static void main(String[] args) {
+        studDetail("alok singh",25,5,"A");
+    }
+}
