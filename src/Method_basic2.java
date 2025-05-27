6907ea28304bb7344101fdcecc6df6b51e916a5e
Asus <singhalokkumar427@gmail.com>
2025-05-27 15:25:08 +0530

    basic 2

---
+++ src/Method_basic2.java
@@ -0,0 +1,10 @@
+public class Method_basic2 {
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
+
